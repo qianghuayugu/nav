bca2d0205005bcbc824819911413cbf66c8aad74
qianghuayugu <a2545214812@gmail.com>
2025-09-02 19:20:18 +0000

    Deploying to gh-pages from @ qianghuayugu/nav@1e4f8fc52605231389c0c46f834385e4e4e83b5b 🚀

--- chunk-J5HZBTOY.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"./chunk-52F3W3EI.js";import{qb as r,zb as e}from"./chunk-DJ2V5INL.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
