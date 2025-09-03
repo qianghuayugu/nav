ba7858fd3eda834de27095436507a8ce6d84c84a
qianghuayugu <a2545214812@gmail.com>
2025-09-03 17:25:18 +0000

    Deploying to gh-pages from @ qianghuayugu/nav@b0f0cf3b1570ce234fc301c7dc47ecfbc712b2b3 🚀

---
+++ chunk-IIP46IUX.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-HMO3ZBLY.js";import{qb as r,zb as e}from"./chunk-LCYTS3KC.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
